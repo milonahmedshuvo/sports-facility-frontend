d018db98335e02885a5c439db771bddacb97cd02
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2024-10-03 13:45:08 +0600

    box shadow color

--- src/components/banner/BannerSection.tsx
+++ src/components/banner/BannerSection.tsx
@@ -2,17 +2,26 @@
 
 const BannerSection = () => {
     return (
-        <div className="w-full">
-            <video controls autoPlay loop muted>
+        <div className="w-full ">
+
+            <video autoPlay loop muted >
                 <source src='https://www.catapult.com/wp-content/uploads/2023/06/HOME-SLIDER-2023-SITE-LAUNCH.mp4' type="video/mp4" />
             </video>
 
-            <div className="w-full h-[200px] bg-[#000000] trans flex flex-col justify-center ">
-               
-               <button className="text-white border border-orange-600 hover:bg-[#EE4D24] w-[300px] text-xl mx-auto py-4">Book Now</button>
-               <p className="uppercase text-white text-center text-4xl font-bold">our solutions</p>
-              
+
+
+
+            <div className="relative w-full h-[200px] bg-[#000000] ">
+
+                <div className="color-spred absolute top-0 left-[-10px] flex flex-col justify-center">
+                    <button className="text-white border border-orange-600 hover:bg-[#EE4D24] w-[300px] text-xl mx-auto py-4 mt-6 ">Book Now</button>
+                    <p className="uppercase text-white text-center text-4xl font-bold mt-4">our solutions</p>
+                </div>
+
+
             </div>
+
+
         </div>
     )
 }
